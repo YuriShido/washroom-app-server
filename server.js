2baa75aaa159ed10f383ce4de373bbac069235de
YuriShido <sydonce57@gmail.com>
2021-07-30 11:22:28 -0700

    cors change'

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 require('dotenv').config()
 const express = require('express')
-// const bodyParser = require('body-parser');
-const cors = require("cors");
+// const cors = require("cors");
 const mongoose = require('mongoose')
 const washroomRouter = require('./routes/washroom.route')
 
 const app = express()
 
 // app.use(cors());
-app.use(cors({
-    origin: 'http://localhost:3000', //アクセス許可するオリジン
-    credentials: true, //レスポンスヘッダーにAccess-Control-Allow-Credentials追加
-    optionsSuccessStatus: 200 //レスポンスstatusを200に設定
-}))
+// app.use(cors({
+//     origin: {'http://localhost:3000', ''}, //アクセス許可するオリジン
+//     credentials: true, //レスポンスヘッダーにAccess-Control-Allow-Credentials追加
+//     optionsSuccessStatus: 200 //レスポンスstatusを200に設定
+// }))
+
+app.use((req, res, next) => {
+    const allowedOrigins = ['http://localhost:3000', 'https://run-van-toilet.netlify.app/'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+         res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
+    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', true);
+    return next();
+  });
 
 // parse requests of content-type - application/json
 app.use(express.json());
@@ -20,7 +32,7 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/data", (req, res) => {
+app.get("/", (req, res) => {
     res.json({ message: "Hello World!" });
 });
 
